ff368e1a35bd9bd2c4da1b3b3a8dc3ed5acf362e
Mohamed Abdullah <63874767+mohamed-abdullah1@users.noreply.github.com>
2023-08-18 16:31:17 +0300

    setup electron-react app

---
+++ src/Components/About.tsx
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div>
+      <p>asdasdasdasd</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/Components/Home.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      <p>asdasd</p>
+      <Link to="/about">About</Link>
+    </div>
+  );
+}
+
+export default Home;
